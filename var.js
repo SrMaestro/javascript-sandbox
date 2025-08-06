a15e915c46333793d5c500ad9bead83fb6e4cf6d
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-08-06 16:28:51 -0300

    Uso de variaveis

---
+++ var.js
@@ -0,0 +1,15 @@
+// Declara um variavel sem um valor
+var user 
+
+console.log(user); //Underfined
+
+
+// Declara uma variavel com valor
+
+let nome = "Bruno"
+console.log(nome); //Bruno
+
+// Substitui o valor da variavel
+
+nome = "Ana"
+console.log(nome); //Ana
